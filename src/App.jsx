29a1068964834113ce120eef5a93a9ea28b510c4
Becky-234 <kiraborebecca04@gmail.com>
2026-06-10 17:02:05 +0300

    Add Education

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
+import Education from './components/Education'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import './App.css'
@@ -86,6 +87,7 @@ function App() {
         <About />
         <Skills />
         <Projects />
+        <Education />
         <Contact />
       </main>
       <Footer />
