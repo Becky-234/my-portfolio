b7ebadd3331e687d16e03514a1868f64261a6f3f
Becky-234 <kiraborebecca04@gmail.com>
2026-07-23 09:32:06 +0300

    Rename Education to Certifications

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
-import Education from './components/Education'
+import Certifications from './components/Certifications'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import './App.css'
@@ -87,7 +87,7 @@ function App() {
         <About />
         <Skills />
         <Projects />
-        <Education />
+        <Certifications />
         <Contact />
       </main>
       <Footer />
