ec86312b56e91d3bfec00f2303633b750c29d595
Becky-234 <kiraborebecca04@gmail.com>
2026-07-27 01:25:14 +0300

    Fix certification link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,7 +31,7 @@ function Footer() {
               <li><a href="#about">About</a></li>
               <li><a href="#skills">Skills</a></li>
               <li><a href="#projects">Projects</a></li>
-              <li><a href="#education">Certifications</a></li>
+              <li><a href="#certifications">Certifications</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul>
           </div>
